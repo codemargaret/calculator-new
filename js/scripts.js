46a19fa996483ab762ae6fae3e9fcdd524ea5119
Dan Danilyuk and Margaret Berry <margaret@>
2017-08-10 15:45:05 -0700

    consolidate functions in scripts.js and add radio buttons to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business (or back-end) logic:
+ // Business (or back-end) logic:
 
 var add = function(number1, number2) {
   return number1 + number2;
@@ -18,33 +18,22 @@ var divide = function(number1, number2) {
 
 // Everything below this line is user interface (or front-end) logic:
 
-$(document).ready(function(event) {
-  $("form#add").submit(function(event) {
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-  });
-  $("form#subtract").submit(function(event) {
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
-    event.preventDefault();
-  });
-  $("form#multiply").submit(function(event) {
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-  $("form#divide").submit(function(event) {
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
   });
 });
